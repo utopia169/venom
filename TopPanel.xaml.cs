049d209ffb923023b2c5c8e7335f997645b9e0c8
Иван Кравчук <ivankravkhuk228335@gmail.com>
2026-02-20 19:45:21 +0300

    was modificated add train button

--- TopPanel.xaml.cs
+++ TopPanel.xaml.cs
@@ -9,20 +9,29 @@ namespace MyApp
         {
             InitializeComponent();
         }
-        private void AddTrain_Click(object sender, RoutedEventArgs e)
-        {
-            var mainWindow = Window.GetWindow(this) as MainWindow;
-            if (mainWindow != null)
-            {
-                mainWindow.Trains.Add(new TrainCard
-                {
-                    Name = $"Train № {mainWindow.Trains.Count + 1}"
-                });
-            }
-        }
         private void UserBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             LoginPopup.IsOpen = true;
+        }
+        private void AddTrainClick(object sender, RoutedEventArgs e) {
+        CreateTrainPopup.IsOpen = true;
+        }
+
+        private void ConfirmAddTrain_Click(object sender, RoutedEventArgs e)
+        {
+            string newTrainName = TrainNameTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(newTrainName))
+            {
+                var mainWindow = Window.GetWindow(this) as MainWindow;
+                if (mainWindow != null)
+                    mainWindow.Trains.Add(new TrainCard
+                    { Name = newTrainName });
+                TrainNameTextBox.Text = string.Empty;
+                CreateTrainPopup.IsOpen = false;
+            }
+            //else {
+            //    MessaageBox.Show("fdds");
+            //}
         }
     }
 }

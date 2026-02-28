1e392ff5050f90262f0016f237d4fa9b3cea9666
Иван Кравчук <ivankravkhuk228335@gmail.com>
2026-02-28 11:20:23 +0300

    modifficated top panel and appearence card. Created logic delte card

--- Content.xaml.cs
+++ Content.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MyApp
 {
@@ -7,6 +8,30 @@ namespace MyApp
         public Content()
         {
             InitializeComponent();
+        }
+
+        private void DeleteTrain_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            TrainCard trainToDelete = btn.DataContext as TrainCard;
+            if (trainToDelete != null)
+            {
+                var result = MessageBox.Show($"Delete train for {trainToDelete.Date}?",
+                    "Confirm", 
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes) 
+                {
+                    var mainWindow = Window.GetWindow(this) as MainWindow;
+
+                    if (mainWindow != null)
+                    {
+                        mainWindow.Trains.Remove(trainToDelete);
+                    }
+                }
+            }
+
         }
     }
 }

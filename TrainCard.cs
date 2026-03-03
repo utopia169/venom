7430bb5e00d20420949d57713ead962678eaf2ba
Иван Кравчук <ivankravkhuk228335@gmail.com>
2026-03-03 11:00:46 +0300

    has been modifficated appearence card menu

--- TrainCard.cs
+++ TrainCard.cs
@@ -1,9 +1,34 @@
-﻿namespace MyApp
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyApp
 {
     public class TrainCard
     {
         public string Date { get; set; }
         public string Type { get; set; }
         public string Emoji { get; set; }
+        public string Duration { get; set; }
+        public ObservableCollection<ExercisesItem> Exercises { get; set; } = new ObservableCollection<ExercisesItem>();
+    }
+
+    public class SetItem : INotifyPropertyChanged
+    {
+        private double _weight;
+        private int _reps;
+
+        public double Weight { get => _weight; set { _weight = value; OnPropertyChanged(); } }
+        public int Reps { get => _reps; set { _reps = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged( [CallerMemberName] string prop = "") =>
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( prop ) );
+    }
+
+    public class ExercisesItem
+    {
+        public string Name { get; set; }
+        public ObservableCollection<SetItem> SetItems { get; set; } = new ObservableCollection<SetItem>();
     }
 }
--- TrainDetailsView.xaml.cs
+++ TrainDetailsView.xaml.cs
@@ -1,14 +1,30 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace MyApp
 {
     public partial class TrainDetailsView : UserControl
     {
         private TrainCard _currentTrain;
+
+        private Dictionary<string, List<string>> _exerciseLibrary = new Dictionary<string, List<string>> 
+        {
+            {"Chest workout", new List<string> {"Bench Press", "Include Duramble Press", "Chest Fly" } },
+            { "Leg workout", new List<string> { "Squats", "Leg Press", "Leg Extension" } },
+            { "Back workout", new List<string> { "Pull-ups", "Deadlift", "Bent Over Row" } },
+            { "Arm workout", new List<string> { "Bicep Curls", "Tricep Dips", "Hammer Curls" } },
+            { "Shoulder workout", new List<string> { "Overhead Press", "Lateral Raise", "Front Raise" } },
+            { "Cardio workout", new List<string> { "Running", "Cycling", "Jump Rope" } }
+        };
+
         public TrainDetailsView(TrainCard train)
         {
             InitializeComponent();
             _currentTrain = train;
+
+            this.DataContext = _currentTrain;
+
+            ExercisesList.ItemsSource  = _currentTrain.Exercises;
 
         }
     }
